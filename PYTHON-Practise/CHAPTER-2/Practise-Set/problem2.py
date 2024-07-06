38038c681e295a1caed607873d69bb53b3374f2e
Yogiraj Chaudhari <yogirajchaudhari55@gmail.com>
2024-07-06 14:23:40 +0530

    Adding the Chapter 2

---
+++ PYTHON-Practise/CHAPTER-2/Practise-Set/problem2.py
@@ -0,0 +1,7 @@
+#Find the remainder when the number is divided by z
+
+a=45
+z=6
+
+print(a%z) # % -> Arithmetic operator Modulus this gives us the remainder of the 2 number
+
---
+++ PYTHON-Practise/CHAPTER-2/operators.py
@@ -0,0 +1,33 @@
+#Arithematic Operators
+
+a = 1
+
+b = 4
+c= a + b
+print(c)
+
+#Assignment Operator
+a=4-2
+b=6
+b+=3  #INcrement the value of b by 3 and then assign it to b
+print(b)
+
+#Comparison Operators(Always returns a Boolean Value)
+a=3
+b=3
+print(a==b)
+
+#Logical Operators
+
+e=True and False # For AND When Both true are present then only it is true.
+#Truth Table of 'or'
+print('True or False is ',True or False)
+print('True or True is ',True or True)
+print('False or True is ',False or True)
+print('False or False is ',False or False)
+
+#Truth Table of 'and'
+print('True and False is ',True and False)
+print('True and True is ',True and True)
+print('False and True is ',False and True)
+print('False and False is ',False and False)
